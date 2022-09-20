53cbbfd2c39eeddb9dcd654619d8bbd259d00ab1
+ <+>
2022-09-20 19:25:06 -0400

    ternary

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance) {
+  
+  if (distance <= 400) {
+    return 'This one is on me!'
 
-function ternaryCheckCity(){
-  // Write your code here!
+  } else if (distance > 400 && distance < 2000) {
+    return 'That will be twenty bucks.'
+
+  } else if (distance > 2000 && distance < 2500) {
+    return 'I will gladly take your thirty bucks.'
+
+  } else if (distance > 2500) {
+     return 'No can do.'
+  }
+
+}
+   
+
+function ternaryCheckCity(city) {
+  const answer = city == "NYC" ? "Ok, sound good." : "No go."
+  return answer // Write your code here!
+
 }
 
 function switchOnCharmFromTip(){
+  
   // Write your code here!
 }
